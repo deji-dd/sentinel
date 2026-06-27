acd7e546b36227ff6ada328f5493562e64312ef3
Ayodeji <dejib50@gmail.com>
2026-06-27 01:22:37 +0100

    chore: remove wrangler configuration, update pnpm version, and configure turbopack root for next-ui

--- apps/next-ui/next.config.ts
+++ apps/next-ui/next.config.ts
@@ -1,8 +1,12 @@
 import type { NextConfig } from "next";
+import path from "path";
 
 const nextConfig: NextConfig = {
   /* config options here */
   allowedDevOrigins: ["macbook-pro.taile7ef20.ts.net"],
+  turbopack: {
+    root: path.resolve(__dirname, "../../"),
+  },
 };
 
 if (process.env.NODE_ENV !== "production") {
